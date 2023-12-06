983b81b6294615a261943a7e5b2ae15a65a9d9d4
Krishna Kashyap <krishnakashyap0704@gmail.com>
2023-12-06 12:02:01 +0530

    modified:   package-lock.json modified:   package.json modified:   src/App.js new file:   src/Components/Footer.jsx new file:   src/Components/NavigationBar.jsx new file:   src/Image/logo.png new file:   src/Image/upNav.png

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,80 @@
+import { MDBFooter, MDBContainer, MDBCol, MDBRow,} from "mdb-react-ui-kit";
+
+export function Footer() {
+    return (
+        <MDBFooter className='bg-secondary text-white text-center text-md-start'>
+            <MDBContainer className='p-4'>
+                <MDBRow>
+                    <MDBCol lg="6" md="12" className='mb-4 mb-md-0'>
+                        <h5 className='text-uppercase'>Safety First</h5>
+
+                        <p>
+                        "Navigate life's roads with care, not speed. Safety is the ultimate destination. Buckle up, stay focused, and drive responsibly."
+                        </p>
+                    </MDBCol>
+
+                    <MDBCol lg="3" md="6" className='mb-4 mb-md-0'>
+                        <h5 className='text-uppercase'>Terms And Policies</h5>
+
+                        <ul className='list-unstyled mb-0'>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Privacy Policy
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Hyperlink Policy
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Website Policies
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Content Policies
+                                </a>
+                            </li>
+                        </ul>
+                    </MDBCol>
+
+                    <MDBCol lg="3" md="6" className='mb-4 mb-md-0'>
+                        <h5 className='text-uppercase mb-0'>Need Help</h5>
+
+                        <ul className='list-unstyled'>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Contact Us
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Frequently Asked Question
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Raise a concern
+                                </a>
+                            </li>
+                            <li>
+                                <a href='#!' className='text-white'>
+                                    Feedback
+                                </a>
+                            </li>
+                        </ul>
+                    </MDBCol>
+                </MDBRow>
+            </MDBContainer>
+
+            <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                © 2023 :{" "}
+                <a className='text-white' href='#!'>
+                     CarDrivingAndRTOservices.com
+                </a>
+            </div>
+        </MDBFooter>
+    );
+}
---
+++ src/Components/NavigationBar.jsx
@@ -0,0 +1,82 @@
+import { Col, Container, Form, Nav, NavDropdown, Navbar, Row } from "react-bootstrap";
+
+export function NavigationBar() {
+    return (
+        <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src="./Image/logo.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}RTO</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">About Us</Nav.Link>
+                        <NavDropdown title="Online Services" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/1.1">Vechile Related Service</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.2">Permit Related Service</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.3">Driving Licence</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.4">Check Post Tax</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.5">Fancy Number Booking</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.6">NR Services</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.7">National Permit Authorization</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.8">PUCC</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.9">eChallan</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.10">Vehicle Scrapping</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/1.11">Vechile Fitness Testing</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Informational Services" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/2.1">Know Your License Details</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.2">Know Your Vechile Details</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.3">About Licencing</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.4">About Registration</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.5">About Permits</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.6">Act, Rules and Policies</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.7">Fees & User Charges</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.8">Notification And Advisories</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/2.9">Downloadable Forms</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Dashboard & Report" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Analytics</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Vahan Report</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Sarathi Report</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">VLTD Dashboard</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.5">Homologation Dashboard</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.6">SLD Dashboard</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.7">eChallan Dashboard</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.8">Trade Certificate Report</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.9">PUCC Dashboard</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="External Links" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/4.1">Delhi Traffic Police (Notice)</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/4.2">MoRTH</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/4.3">National Informatics Centre</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#Sitemap">Sitemap</Nav.Link>
+                        <Nav.Link href="#Contactus">Contact Us</Nav.Link>
+                        <NavDropdown title="Login" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/5.1">Sign In</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/5.2">Registration</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/5.3">Support</NavDropdown.Item>
+                        </NavDropdown>
+                        <Form inline>
+                            <Row>
+                                <Col xs="auto">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Search"
+                                        className=" mr-sm-2"
+                                    />
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
